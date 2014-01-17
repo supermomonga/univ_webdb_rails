9a326f223c000322a1235d14294b3a3019b2597d
supermomonga <hi@supermomonga.com>
2014-01-18 01:18:52 +0900

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,17 @@
 
 sudo cat <<EOF | sudo tee /media/state/units/docker-daemon.service
 [Unit]
-Description=Docker Application Container Engine 
+Description=Docker Application Container Engine
 Documentation=http://docs.docker.io
 After=docker.service
+Requires=docker.service
 
 [Service]
+Restart=always
+RetartSec=10s
 ExecStartPre=systemctl stop docker
-# Run docker but don't have docker automatically restart
-# containers. This is a job for systemd and unit files.
 ExecStart=/usr/bin/docker -d -r=false -H tcp://0.0.0.0:4243
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=local.target
 EOF
